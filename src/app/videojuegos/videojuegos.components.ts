1df60443ee5c67b40c7a412e5d96a5bc5cd99073
Bryan Astacio <brya0196@gmail.com>
2018-04-03 22:20:09 -0400

    First Commit

---
+++ src/app/videojuegos/videojuegos.components.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'videojuegos',
+    template: `
+        <h2>Componente de {{ nombre }}</h2>
+        <h3 *ng-if="mostrarRetro != true">El mejor juego es: {{ mejorJuego }}</h3>
+        <h3 [style.background]="yellow"  
+            *ng-if="mostrarRetro">
+            El mejor juego retro es: {{ mejorJuegoRetro }}
+        </h3>
+    `
+})
+
+export class VideojuegosComponent {
+    public nombre = 'Videojuegos 2018'
+    public mejorJuego = 'GTA 5';
+    public mejorJuegoRetro = "Super Mario 64";
+    public mostrarRetro = true;
+}
